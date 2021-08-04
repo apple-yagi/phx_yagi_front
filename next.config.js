b1d5205c94af1a0b8425dcc38c6faca4ab3235c2
apple-yagi <applesryu@gmail.com>
2021-08-05 00:50:52 +0900

    add md-editor

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
-module.exports = {
+const withPlugins = require("next-compose-plugins");
+const removeImports = require("next-remove-imports")();
+
+module.exports = withPlugins([removeImports({})], {
   images: {
     domains: ["cdn.svgporn.com", "iconbu.com", "d1d9yfqtve2e82.cloudfront.net"]
   }
-};
+});
--- src/pages/[id]/articles/[articleId].tsx
+++ src/pages/[id]/articles/[articleId].tsx
@@ -1,7 +1,10 @@
 import { GetServerSideProps, NextPage } from "next";
+import MDEditor from "@uiw/react-md-editor";
 import { API_BASE_URL } from "~/constants";
 import { Container } from "~/styles/common";
 import { Article } from "~/types";
+import "@uiw/react-md-editor/dist/markdown-editor.css";
+import "@uiw/react-markdown-preview/dist/markdown.css";
 
 type Props = {
   article: Article;
@@ -28,6 +31,7 @@ const ArticlePage: NextPage<Props> = ({ article }) => {
   return (
     <Container>
       <h1>{article.title}</h1>
+      <MDEditor.Markdown source={article.content} />
     </Container>
   );
 };
